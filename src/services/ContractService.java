707a6b9d7be84cc3c28194d411989f362d452932
Lucas Fortuna <lucasfortuna89@outlook.com>
2025-06-02 15:12:54 -0300

    Parte 1

---
+++ src/services/ContractService.java
@@ -0,0 +1,20 @@
+package services;
+
+import entities.Contract;
+import entities.Installment;
+
+import java.time.LocalDate;
+
+public class ContractService {
+
+    private OnlinePaymentService onlinePaymentService;
+
+    public ContractService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+   public void processContract(Contract contract, int months) {
+
+        contract.getInstalments().add(new Installment(LocalDate.of(2018, 7, 25),206.40));
+        contract.getInstalments().add(new Installment(LocalDate.of(2018, 7, 25),206.40));
+   }
+}
