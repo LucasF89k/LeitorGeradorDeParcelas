d79110f220740e0357f11ffa0ee8af8eeb3a8130
Lucas Fortuna <lucasfortuna89@outlook.com>
2025-06-09 14:54:50 -0300

    Parte 2

--- src/services/ContractService.java
+++ src/services/ContractService.java
@@ -14,7 +14,17 @@ public class ContractService {
     }
    public void processContract(Contract contract, int months) {
 
-        contract.getInstalments().add(new Installment(LocalDate.of(2018, 7, 25),206.40));
-        contract.getInstalments().add(new Installment(LocalDate.of(2018, 7, 25),206.40));
+        double basicQuota = contract.getTotalValue()/ months;
+
+        for (int i=1; i <= months; i++) {
+            LocalDate dueDate = contract.getDate().plusMonths(i);
+
+            double interest = onlinePaymentService.interest(basicQuota, i);
+            double fee = onlinePaymentService.paymentFee(basicQuota + interest);
+            double quota = basicQuota + interest + fee;
+
+            contract.getInstalments().add(new Installment(dueDate, quota));
+
+        }
    }
 }
